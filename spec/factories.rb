3550e946b6c2dd45f8961f0ef1517376979bb771
Dane Jorgensen <dane@bgbng.com>
2013-01-15 12:23:16 -0700

    updates to spec/factories to reflect model changes

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,12 +3,10 @@ FactoryGirl.define do
     purchase_order_id { FactoryGirl.create(:purchase_order) }
     purchase_order_item_id { FactoryGirl.create(:purchase_order_item) }
     purchase_order_item_type_id { FactoryGirl.create(:purchase_order_item_type) }
-    sequence(:account_id) { |n| "#{n}" }
     quantity 1
-    sequence(:notes) { |n| "Agreement Notes #{n}" } 
     sequence(:exuberance_coordinator_id) { |n| "Exuberance Coordinator #{n}" } 
-    start_date Date.new
-    end_date Date.new
+    start_date Date.today
+    end_date Date.today
     funding_source 1
     status_id 1
     deleted false
@@ -62,21 +60,17 @@ FactoryGirl.define do
 
   factory :opportunity do
     agreement_id { FactoryGirl.create(:agreement) }
-    created_date Date.new
+    created_date Date.today
   end
 
   factory :purchase_order do
-    sale_id 1
-    account_id "1234"
-    purchase_order_date Date.new
+    sale_number 1
+    purchase_order_date Date.today
     number "123"
-    amount 10.00
-    discount 0.00
-    tax 1.00
     invoice_number "12345"
-    area_sales_manager_id "Billy Bob APM"
+    apm "Billy Bob APM"
     region_id "1"
-    field_technician_id "Billy Bob Field"
+    sts "Billy Bob Field"
     sequence(:notes) { |n| "Purchase Order Notes #{n}" }
     deleted false
     created_by "Billy Bob"
